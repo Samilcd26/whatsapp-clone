d0788e614bb325fd729cbd24c67ee5ae49f87b94
samil <samilcd.26@outlook.com>
2023-02-28 22:23:18 +0300

    Login page complate

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {Whatsapp} from 'react-bootstrap-icons'
 export default function Login() {
-    const url = "https://xsgames.co/randomusers/avatar.php?g="
-    let sex = "male";
-    // url=sex==="male"?url+"male":url+"female"
+    const url = "https://randomuser.me/api/?gender="
+    const [gender,setGender]=useState("male");
+    const [imageUrl,setImageUrl]=useState("");
+    const male=useRef();
+    const female=useRef();
+    useEffect(()=>{
 
-    // useEffect(()=>{
-    //     fetch(url)
-    // .then((response) => response.json())
-    // .then((res)=>console.log(res))
-    // },[])
+        fetch(url+gender)
+    .then((response) => response.json())
+    .then((res)=>setImageUrl(res.results[0].picture.large))
+    },[gender])
+
+
+   const maleHandle=()=>{
+    setGender("male")
+    male.current.checked=true
+    female.current.checked=false
+
+   }
+   const femaleeHandle=()=>{
+    setGender("female")
+    male.current.checked=false
+    female.current.checked=true
+    
+   }
 
     return (
         <div className='bg-black-whatsapp h-screen'>
@@ -27,35 +43,31 @@ export default function Login() {
 
                     <div className='w-full h-3/4 bg-white rounded-sm grid grid-cols-2 '>
 
-                        <div className='col-span-1'>
+                        <div className='col-span-1 flex items-center'>
                             <div className='p-16 '>
                                 {/* Name input */}
-                                <div className='flex'>
-                                    <p className='w-12 '>Ad:</p>
-                                    <input type='text' className='ml-3 bg-grean-whatsapp text-center focus:outline-none focus:bg-teal-500 text-white rounded-full' />
+                                <div className='flex flex-col'>
+                                    <p className='text-center'>Name</p>
+                                    <input type='text' className='w-full ml-3 bg-grean-whatsapp text-center focus:outline-none focus:bg-teal-500 text-white rounded-full' />
                                 </div>
-                                {/* Name input */}
-                                <div className=' flex mt-10'>
-                                    <p className='w-12'>Soyad:</p>
-                                    <input type='text' className='ml-3 bg-grean-whatsapp text-center focus:outline-none focus:bg-teal-500 text-white rounded-full' />
-                                </div>
+                                
 
-                                <button className='mt-10 bg-grean-whatsapp w-full py-3 rounded-full text-white hover:bg-teal-500'>Giriş</button>
+                                <button className='mt-10 bg-grean-whatsapp w-full py-3 rounded-full text-white hover:bg-teal-500'>Login</button>
                             </div>
                         </div>
 
                         <div className='col-span-1  flex mt-28 mx-auto flex-col'>
-                            <div className=' w-56 h-56'>
-                                <img src='https://xsgames.co/randomusers/assets/avatars/male/25.jpg'></img>
+                            <div className='w-56 h-56 flex items-center justify-center'>
+                                <img src={imageUrl}></img>
                             </div>
                             <div className='flex flex-row justify-between py-2 '>
                                 <div className='text-center'>
-                                    <p>Erkek</p>
-                                    <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                    <p>Male</p>
+                                    <input onClick={maleHandle} ref={male} id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                                 </div>
                                 <div className='text-center default-checkbox'>
-                                    <p>Kadın</p>
-                                    <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                    <p>Female</p>
+                                    <input onClick={femaleeHandle} ref={female} id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                                 </div>
                             </div>
                         </div>
